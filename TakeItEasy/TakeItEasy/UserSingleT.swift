3145b931081d0368f54dae6ffbd56cd5608daf59
philip713 <105677806+philip713@users.noreply.github.com>
2022-06-12 23:24:54 -0600

    Music API/Singleton
    Added file for music api models, MusicSearchManager to get music files from itunes (Can only get previews) and a singleton class for username

---
+++ TakeItEasy/TakeItEasy/UserSingleT.swift
@@ -0,0 +1,17 @@
+//
+//  UserSingleT.swift
+//  TakeItEasy
+//
+//  Created by Philip Janzel Paradeza on 2022-06-12.
+//
+
+import Foundation
+class UserSingleT{
+    static var userData = UserSingleT()
+    private var currentUsername = "" // set the successfully loggedin username here at login/register for the Navbar's Username
+    private init(){
+        
+    }
+    
+}
+
---
+++ TakeItEasy/TakeItEasy/ViewModels/MusicSearchManager.swift
@@ -0,0 +1,56 @@
+//
+//  MusicSearchManager.swift
+//  TakeItEasy
+//
+//  Created by Philip Janzel Paradeza on 2022-06-12.
+
+import Foundation
+import MusicKit
+class MusicSearchManager{
+    private var songList = [Songs]() //
+    
+    init(){
+        loadSongs()
+    }
+    //Can't use AVFoundation to play songs from remote sourrce can use AVPlayer instead
+    //Populated songList at instantiation
+    func loadSongs(){
+        //JSON file from itunes
+        let url = URL(string: "https://itunes.apple.com/search?term=nirvana&media=music&entity=song&musicTrack")!
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+          if let error = error {
+            print(error)
+          }
+            guard let myData = data else {
+                return
+            }
+            do {
+                let response = try JSONDecoder().decode(SongResponse.self, from: myData)
+                //print(response.results)
+                self.songList = response.results
+
+            } catch { print(error) }
+        }
+        .resume()
+    }
+    
+    
+    //Call when user searches a song
+    //Return an array of objects Songs based on search parameter
+    //songs has trackName, artistName, trackId, and previewUrl (check API Models)
+    //use previewUrl to play track
+    func searchSongs(searchParameter : String) -> [Songs]{
+        if(searchParameter == ""){
+            return songList
+        }
+        var searchResult = [Songs]()
+        var i = 0
+        while(i < songList.count){
+            if(songList[i].trackName.lowercased().contains(searchParameter.lowercased()) || songList[i].artistName.lowercased().contains(searchParameter.lowercased())){
+                searchResult.append(songList[i])
+            }
+            i += 1
+        }
+        return searchResult
+    }
+}
