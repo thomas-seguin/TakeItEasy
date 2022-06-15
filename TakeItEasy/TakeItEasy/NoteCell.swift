0e111719c8eaa5e31ff85beec7c70a39188f769c
satak <saigham@mcmaster.ca>
2022-06-15 19:17:37 -0400

    Completed Changes
    June 15, 2022

---
+++ TakeItEasy/TakeItEasy/NoteCell.swift
@@ -0,0 +1,27 @@
+//
+//  NoteCell.swift
+//  TakeItEasy
+//
+//  Created by MAC on 2022-06-15.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell {
+   
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    
+    
+    
+}
