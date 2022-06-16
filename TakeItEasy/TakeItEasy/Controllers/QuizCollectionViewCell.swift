5d078da621908c41197bdcbca48e5355b01e6d8b
philip713 <105677806+philip713@users.noreply.github.com>
2022-06-16 10:59:22 -0600

    update quiztab

---
+++ TakeItEasy/TakeItEasy/Controllers/QuizCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  QuizCollectionViewCell.swift
+//  TakeItEasy
+//
+//  Created by Philip Janzel Paradeza on 2022-06-16.
+//
+
+import UIKit
+
+class QuizCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var quizLabel : UILabel!
+}
--- TakeItEasy/TakeItEasy/Controllers/QuizViewController.swift
+++ TakeItEasy/TakeItEasy/Controllers/QuizViewController.swift
@@ -7,11 +7,44 @@
 
 import UIKit
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate{
 
+    //Username and Logout Bar
+    @IBOutlet weak var customBar: UINavigationBar!
+    var newQuizViewModel = QuizViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        customBar.topItem?.title = UserSingleton.userData.currentUsername
+    }
+ 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return newQuizViewModel.quizzes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! QuizCollectionViewCell
+        cell.quizLabel.text = newQuizViewModel.quizzes[indexPath.count].quizName
+        cell.backgroundColor = UIColor.purple
+        cell.layer.borderColor = CGColor(red: 100, green: 0, blue: 255, alpha: 1)
+        cell.layer.cornerRadius = 10.0
+        cell.layer.borderWidth = 1.0;
+        cell.layer.masksToBounds = true;
+        return cell
+    }
+    
+    
+    
+//MARK: For Testing purposes
+    func testingQuestionaires(){
+        for quiz in newQuizViewModel.quizzes{
+            print(quiz.quizId!,quiz.quizName!,quiz.totalScore)
+            for question in quiz.questions!{
+                print(question.quizId! ,question.questionid!, question.questionName!, question.score!)
+                for answer in question.answers!{
+                    print(answer.questionId!, answer.answerId!, answer.answerName!, answer.isCorrect!)
+                }
+            }
+        }
     }
     
 }
