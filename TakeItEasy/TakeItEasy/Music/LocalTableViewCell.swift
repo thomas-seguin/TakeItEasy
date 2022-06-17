145c27c56c5571e683133ec4c709855396ee1b63
Djay <72912812+djay-roy@users.noreply.github.com>
2022-06-18 03:08:39 +0530

    Pre Final

---
+++ TakeItEasy/TakeItEasy/Music/LocalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocalTableViewCell.swift
+//  TakeItEasy
+//
+//  Created by Dhananjay H. Roy on 2022-06-16.
+//
+
+import UIKit
+
+class LocalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TakeItEasy/TakeItEasy/Music/MusicData.swift
@@ -0,0 +1,8 @@
+//
+//  MusicData.swift
+//  TakeItEasy
+//
+//  Created by Dhananjay H. Roy on 2022-06-17.
+//
+
+import Foundation
