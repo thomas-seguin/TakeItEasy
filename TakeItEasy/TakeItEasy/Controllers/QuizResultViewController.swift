cc0059488c46713624e039a6ef44613419ad85a4
philip713 <105677806+philip713@users.noreply.github.com>
2022-06-17 14:29:37 -0600

    QuizTab Files

---
+++ TakeItEasy/TakeItEasy/Controllers/QuizResultViewController.swift
@@ -0,0 +1,48 @@
+//
+//  QuizResultViewController.swift
+//  TakeItEasy
+//
+//  Created by Philip Janzel Paradeza on 2022-06-16.
+//
+
+import UIKit
+
+class QuizResultViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var prevView = QuestionaireViewController()
+    @IBOutlet weak var resultHeader: UILabel!
+    @IBOutlet weak var resultImg: UIImageView!
+    @IBOutlet weak var scoreLbl: UILabel!
+    @IBOutlet weak var pointsLbl: UILabel!
+    @IBOutlet weak var codeTxtField: UITextField!
+    var newQuizResultViewModel : QuizResultViewModel?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if(newQuizResultViewModel!.didUserPass()){
+            resultHeader.text = "Congratulations!"
+            resultImg.image = UIImage(named: "passed-stamp-round-grunge-sign-label-181912691")
+        }
+        newQuizResultViewModel?.playSound()
+        let num = String(Int(newQuizResultViewModel!.getScore()))
+        scoreLbl.text = "Quiz Score: " + num + "%"
+        pointsLbl.text = "Copy/Save and use this code at the FunZoneShop to redeem" + num + " points"
+        codeTxtField.text = newQuizResultViewModel?.getCode()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newQuizResultViewModel!.getArrayCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ReviewTableViewCell
+        cell.questionLbl.text = newQuizResultViewModel?.questionNames[indexPath.row]
+        cell.answerLbl.text = newQuizResultViewModel?.correctAnswers[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+        prevView.dismiss(animated: false)
+    }
+
+}
--- TakeItEasy/TakeItEasy/Controllers/QuizViewController.swift
+++ TakeItEasy/TakeItEasy/Controllers/QuizViewController.swift
@@ -7,11 +7,74 @@
 
 import UIKit
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate{
 
+    //Username and Logout Bar
+    @IBOutlet weak var customBar: UINavigationBar!
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    @IBOutlet weak var retakeWarning: UILabel!
+    var newQuizViewModel = QuizViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
+        customBar.topItem?.title = UserSingleton.userData.currentUsername
         
+//        let test = QuizDBHelper.dbHelper.getAllUserResults(username: UserSingleton.userData.currentUsername as NSString)
+//        print(test)
+//        for x in test{
+//            print(x.resultId,x.username,x.quizId,x.userScore)
+//        }
+    
+    }
+ 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return newQuizViewModel.searchedQuizzes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! QuizCollectionViewCell
+        cell.quizLabel.text = newQuizViewModel.searchedQuizzes[indexPath.row].quizName
+        cell.backgroundColor = UIColor.purple
+        cell.layer.borderColor = CGColor(red: 100, green: 0, blue: 255, alpha: 1)
+        cell.layer.cornerRadius = 10.0
+        cell.layer.borderWidth = 1.0;
+        cell.layer.masksToBounds = true;
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        newQuizViewModel.getSearchedQuizzes(searchText: searchText)
+        myCollectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if(!newQuizViewModel.haveUserDoneQuiz(index : indexPath.row))
+        {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let questionaire = storyboard.instantiateViewController(withIdentifier: "Questionaire") as! QuestionaireViewController
+            let obj = QuestionaireViewModel(q : newQuizViewModel.searchedQuizzes[indexPath.row])
+            questionaire.newQuestionaireViewModel = obj
+            print(newQuizViewModel.searchedQuizzes[indexPath.row].quizName!)
+            retakeWarning.isHidden = true
+            present(questionaire, animated: true)
+            
+        }
+        else{
+            retakeWarning.isHidden = false
+        }
+    }
+    
+//MARK: For Testing purposes
+    func testingQuestionaires(){
+        for quiz in newQuizViewModel.quizzes{
+            print(quiz.quizId!,quiz.quizName!,quiz.totalScore)
+            for question in quiz.questions!{
+                print(question.quizId! ,question.questionid!, question.questionName!, question.score!)
+                for answer in question.answers!{
+                    print(answer.questionId!, answer.answerId!, answer.answerName!, answer.isCorrect!)
+                }
+            }
+        }
     }
     
 }
+
