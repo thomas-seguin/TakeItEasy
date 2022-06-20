c9239945a39da48956ea0d6ef3aec8f8b3aaa895
Djay <72912812+djay-roy@users.noreply.github.com>
2022-06-20 07:48:37 +0530

    Final

--- TakeItEasy/TakeItEasy/Music/MusicStreamingViewController.swift
+++ TakeItEasy/TakeItEasy/Music/MusicStreamingViewController.swift
@@ -10,59 +10,57 @@ import AVFoundation
 import WebKit
 
 
-class MusicStreamingViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UISearchResultsUpdating {
-    
-    
+class MusicStreamingViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
     
     var songs = [Song]()
     var player: Player!
     var musicURL: URL!
     
+    @IBOutlet weak var beginTimer: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    
+    var retArray = [String]()
+    
+    var albumImage = ["img 1","img 2","img 3","img 4","img 5","img 6","img 7","img 8","img 9","img 10","img 11"]
+    
+//    var songList:[Int: String] = [56:"Allthat.mp3",57:"Creativeminds.mp3",58:"Dreams.mp3",59:"Elevate.mp3",60:"Funday.mp3",61:"Hey.mp3",62:"Inspire.mp3",63:"Jazzyfrenchy.mp3",64:"Onceagain.mp3",65:"Photoalbum.mp3",66:"Ukulele.mp3"]
+    
+    var songList:[Int: String] = [:]
+    
+    
+    var currentSongName = ""
     
     var mplayer:AVPlayer?
     var playerItem:AVPlayerItem?
-    fileprivate let seekDuration: Float64 = 10
     
-    @IBOutlet weak var lblOverallDuration: UILabel!
-    @IBOutlet weak var lblcurrentText: UILabel!
-    @IBOutlet weak var playbackSlider: UISlider!
+    @IBOutlet weak var volumeControlSlider: UISlider!
     
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var currentSong: UILabel!
     @IBOutlet weak var MusicCollectionView: UICollectionView!
     
-    
-    
-    
-    let searchController = UISearchController()
+    var indexNumber = [Int]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "search"
-        searchController.searchResultsUpdater = self
-        navigationItem.searchController = searchController
         player = Player()
         retrieveSongs()
         
-        let mplay = AVPlayer()
-        if let currentItem = mplay.currentItem {
-            let duration = currentItem.asset.duration
-        }
-            let currenTime = mplay.currentTime()
+//        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+//        volumeControlSlider.transform = CGAffineTransform(rotationAngle: (CGFloat.pi / -2))
         
-        print()
-        
+    }
+    
+    @IBAction func LogOutButton(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "login") as! LoginViewController
+        self.present(nextViewController, animated:true, completion:nil)
     }
     
     @IBAction func volumeControl(_ sender: UISlider) {
         player.avplayer.volume = sender.value
     }
     
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else {
-            return
-        }
-        print(text)
-    }
     
     func retrieveSongs()
     {
@@ -79,13 +77,6 @@ class MusicStreamingViewController: UIViewController,UICollectionViewDelegate,UI
         task.resume()
         print("Getting Songs")
     }
-    
-    @objc func updateTime()
-    {
-       
-
-//        musicSlider.value = Float(audioPlayer!.currentTime)
-    }
 
     func parseSongs(data: String)
     {
@@ -98,9 +89,12 @@ class MusicStreamingViewController: UIViewController,UICollectionViewDelegate,UI
                 let newSong = Song(id: itemData[0], name: itemData[1], likes: itemData[2], plays: itemData[3])
                 songs.append(newSong!)
             }
+            print("IndexNumber",indexNumber)
             for s in songs
             {
                 print(s.getName())
+                songList[s.id] = s.getName()
+                print(songList)
             }
             DispatchQueue.main.async
             {
@@ -116,20 +110,24 @@ class MusicStreamingViewController: UIViewController,UICollectionViewDelegate,UI
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "MusicCell", for: indexPath) as! SongsCollectionViewCell
         
+        cell.albumImage.image = UIImage(named: albumImage[indexPath.row])
         cell.songName.text = songs[indexPath.row].getCleanName()
         
         return cell
     }
     
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         player.playStream(fileUrl: "https://infinitesimal-strob.000webhostapp.com/music_app/" + songs[indexPath.row].getName())
         currentSong.text = songs[indexPath.row].getCleanName()
+        currentSongName = songs[indexPath.row].getName()
+        count = songs[indexPath.row].id
+        print("song id",count)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(getTime), userInfo: nil, repeats: true)
     }
     
     
-    @IBAction func ButtonPlay(_ sender: Any) {
+    @IBAction func playPauseButtonClick(_ sender: Any) {
         if (player.avplayer!.rate > 0)
         {
             player?.pauseAudio()
@@ -140,10 +138,111 @@ class MusicStreamingViewController: UIViewController,UICollectionViewDelegate,UI
         {
             player?.playAudio()
             print("Music Playing")
-            
+            getTime()
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(getTime), userInfo: nil, repeats: true)
         }
+    }
+    
+    var timer : Timer?
+    
+  @objc func getTime()
+    {
+        var audioStartTime: CMTime = CMTimeMake(value: 10, timescale: 1)
+       
+        let currentTime = player.avplayer.currentTime()
+        let runningTime = CMTimeGetSeconds(currentTime)
         
+        let duration = player.avplayer.currentItem!.asset.duration
+        let seconds = CMTimeGetSeconds(duration)
+        let secondsText = Int(seconds) % 60
+        let minutesText = Int(seconds) / 60
+        durationLabel.text = "\(minutesText):\(secondsText)"
+        
+        let cSeconds = Int(runningTime)
+        print("Current",cSeconds)
+        let cMinutes = Int(cSeconds) / 60
+        let cSec = cSeconds - cMinutes * 60
+        beginTimer.text = String(format: "%02d:%02d", cMinutes,cSec) as String
+        progressBar.progress = Float(runningTime)/Float(seconds)
+    }
+    
+        
+    var count = 1
+
+    @IBAction func NextSong(_ sender: Any)
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(getTime), userInfo: nil, repeats: true)
+        print("Current Song", currentSongName)
+        for (key, value) in songList
+        {
+            if count <= songList.keys.min()!
+            {
+                count = songList.keys.max()!
+                print(count)
+            }
+            print(songList.keys.max()!)
+            print(songList.keys.min()!)
+            print("Count before",count)
+            
+            if (currentSongName == songList[count])
+            {
+                print(currentSongName,songList[count]!)
+                count = count - 1
+                player?.pauseAudio()
+                player.playStream(fileUrl: "https://infinitesimal-strob.000webhostapp.com/music_app/"+(songList[count] ?? "")!)
+                print(songList[count] ?? "")
+                currentSongName = ""
+                let cleanName = songList[count]!.dropLast(4)
+                currentSong.text = String(cleanName)
+                currentSongName = songList[count] ?? ""
+                print("Count After",count)
+                print("list",songList[count] ?? "")
+                print(currentSongName,songList[count]!)
+                print("https://infinitesimal-strob.000webhostapp.com/music_app/",songList[count] ?? "")
+                break
+            }
+            break
+        }
     }
     
     
+    @IBAction func PreviousSongButton(_ sender: Any)
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(getTime), userInfo: nil, repeats: true)
+        print("Current Song", currentSongName)
+        for (key, value) in songList
+        {
+            if count > songList.keys.max()!
+            {
+                count = songList.keys.min()!
+                print(count)
+            }
+            print(songList.keys.max()!)
+            print(songList.keys.min()!)
+            print("Count before",count)
+            
+            if (currentSongName == songList[count])
+            {
+                print(currentSongName,songList[count]!)
+                count = count + 1
+                player?.pauseAudio()
+                player.playStream(fileUrl: "https://infinitesimal-strob.000webhostapp.com/music_app/"+(songList[count] ?? "")!)
+                print(songList[count] ?? "")
+                currentSongName = ""
+                let cleanName = songList[count]!.dropLast(4)
+                currentSong.text = String(cleanName)
+                currentSongName = songList[count] ?? ""
+                print("Count After",count)
+                print("list",songList[count] ?? "")
+                print(currentSongName,songList[count]!)
+                print("https://infinitesimal-strob.000webhostapp.com/music_app/",songList[count] ?? "")
+                break
+            }
+            break
+        }
+    }
+    
+
 }
+
+
